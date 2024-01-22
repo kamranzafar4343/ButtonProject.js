fc30f423c58bd8bae72563ba59650f63db7d0069
Kamran Zafar <kamranzafar4343@gmail.com>
2024-01-22 23:34:03 +0500

    added comments

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 var index=0;
 
 function changeColor(){
-    var color=[red, green, violet, purple, blue, yellow];
+    //array of colors
+    var color=["red", "green", "violet", "purple", "blue", "yellow"];
     
+//selects body and changes color while clicking
     document.getElementsByTagName("body")[0].style.background = color[index++];
 
+    
+//resets to 0 index
     if(index > color.length - 1){
         index = 0;
     }
