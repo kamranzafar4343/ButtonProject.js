a7349633199ed936503885f793b05b503c16c755
Kamran Zafar <kamranzafar4343@gmail.com>
2024-01-22 23:27:41 +0500

    added js fun

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 var index=0;
 
 function changeColor(){
-    var color=[red, green, violet, purple, blue, yellow]; 
+    var color=[red, green, violet, purple, blue, yellow];
+    
+    document.getElementsByTagName("body")[0].style.background = color[index++];
+
+    if(index > color.length - 1){
+        index = 0;
+    }
 }
